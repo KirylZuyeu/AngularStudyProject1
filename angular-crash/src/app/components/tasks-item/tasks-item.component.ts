eeb5281aa04937ad6e9f7d6f8e7afdce27c93dea
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-02-04 19:36:36 +0300

    feat: add functionality for deleting task

--- angular-crash/src/app/components/tasks-item/tasks-item.component.ts
+++ angular-crash/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task'
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,12 +8,17 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent implements OnInit {
-  @Input() task!: Task
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task:Task) {
+    this.onDeleteTask.emit(task)
   }
 
 }
