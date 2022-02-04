4001e05c49d77cdb4ef33df483ef2e5c0c66b68f
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-02-04 16:53:59 +0300

    feat: add new component task;

---
+++ angular-crash/src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
